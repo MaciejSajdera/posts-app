dd0988d9a71d5d623a53c61bed577542d6020754
Maciek <msajdera@gmail.com>
2023-02-01 20:28:03 +0100

    Updated: Card styling.

--- src/common/components/ui/Cards/AuthorsCard.tsx
+++ src/common/components/ui/Cards/AuthorsCard.tsx
@@ -20,7 +20,15 @@ export default function AuthorsCard({ ...props }: TAuthorsCard): JSX.Element {
   const hasAlt = authorsAvatar && authorsAvatar?.alt;
 
   return (
-    <Card {...props} sx={{ height: "100%" }}>
+    <Card
+      {...props}
+      sx={{
+        height: "100%",
+        display: "flex",
+        flexFlow: "column",
+        justifyContent: "space-between",
+      }}
+    >
       <Link href={`/user/${author.id}`} style={{ textDecoration: "none" }}>
         <CardActionArea>
           <CardMedia
